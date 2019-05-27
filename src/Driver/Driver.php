1589dd81ef98383ecc470b3347930fd017877acf
Rafal Konieczny <r.konieczny@i-systems.pl>
2019-05-27 11:55:48 +0200

    Added greater-than operation

--- src/Driver/Driver.php
+++ src/Driver/Driver.php
@@ -7,4 +7,6 @@ use Ayeo\Aquerium\Field\Field;
 abstract class Driver
 {
 	abstract function equal(Field $field, string $value): string;
+	
+	abstract function greaterThan(Field $field, string $value): string;
 }
